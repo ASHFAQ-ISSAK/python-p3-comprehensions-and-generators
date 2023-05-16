ef790be2df41d6de860bbed973bd38f1c85fccdb
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-16 11:27:20 +0300

    created functions to pass tests

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    """Returns a list of even numbers from num_list"""
+    evens = []  # Initialize an empty list to store the even numbers
+
+    for num in num_list:  # Iterate over each number in num_list
+        if num % 2 == 0:  # If the number is even (remainder of division by 2 is 0)
+            evens.append(num)  # Add the number to the evens list
+
+    return evens  # Return the list of even numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    """Adds exclamation marks to the end of each sentence in sentence_list"""
+    exclaimed_sentences = []
+
+    for sentence in sentence_list:
+        exclaimed_sentence = sentence + "!"
+        exclaimed_sentences.append(exclaimed_sentence)
+
+    return exclaimed_sentences
